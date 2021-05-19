20db8670584141cb2ba8de7e313a3ea8820df8ee
sunjinkim <sunjinkim@emoney.co.kr>
2021-05-19 17:37:22 +0900

    submit quiz result

--- java8to11/src/main/java/me/whiteship/java8to11/App.java
+++ java8to11/src/main/java/me/whiteship/java8to11/App.java
@@ -2,6 +2,7 @@ package me.whiteship.java8to11;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class App {
@@ -27,7 +28,8 @@ public class App {
 
         System.out.println("수업 이름만 모아서 스트림 만들기");
         // TODO
-
+        Stream<String> stream = springClasses.stream().map(oc -> oc.getTitle());
+        stream.forEach(s -> System.out.println(s));
 
         List<OnlineClass> javaClasses = new ArrayList<>();
         javaClasses.add(new OnlineClass(6, "The Java, Test", true));
@@ -40,14 +42,18 @@ public class App {
 
         System.out.println("두 수업 목록에 들어있는 모든 수업 아이디 출력");
         // TODO
+        keesunEvents.stream().forEach(oc -> oc.stream().map(oc2-> oc2.getId()));
 
         System.out.println("10부터 1씩 증가하는 무제한 스트림 중에서 앞에 10개 빼고 최대 10개 까지만");
         // TODO
 
         System.out.println("자바 수업 중에 Test가 들어있는 수업이 있는지 확인");
         // TODO
+        javaClasses.stream().filter(oc -> oc.getTitle().contains("Test"));
+        javaClasses.stream().anyMatch(oc -> oc.getTitle().contains("test"));
 
         System.out.println("스프링 수업 중에 제목에 spring이 들어간 것만 모아서 List로 만들기");
         // TODO
+        List<OnlineClass> spring = springClasses.stream().filter(oc -> oc.getTitle().contains("spring")).collect(Collectors.toList());
     }
 }
